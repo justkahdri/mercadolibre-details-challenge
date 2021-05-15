a45208699bfb4094018408968966d707d78ec582
Joaquín R. Montes <joaco.rm512@gmail.com>
2021-05-15 15:30:24 -0300

    Cambio de rutas para mostrar solo los detalles de Producto

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,12 +3,10 @@ import {Switch, Route, Redirect} from "react-router-dom";
 
 import DetailsScreen from "../product/screens/Details";
 
-import HomeScreen from "./screens/Home";
-
 const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route exact component={HomeScreen} path="/" />
+      <Route exact component={DetailsScreen} path="/" />
       <Route component={DetailsScreen} path="/:product" />
       <Redirect to="/" />
     </Switch>
