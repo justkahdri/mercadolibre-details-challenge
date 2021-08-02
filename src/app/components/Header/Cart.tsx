5cfa69762f0a3ad5a2ae452dffa8db3747ee172c
Joaquín R. Montes <joaco.rm512@gmail.com>
2021-08-02 16:37:28 -0300

    Atomized cart

---
+++ src/app/components/Header/Cart.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Icon} from "@chakra-ui/react";
+import {CgShoppingCart} from "react-icons/cg";
+
+const Cart = () => {
+  return <Icon as={CgShoppingCart} height={5} width={5} />;
+};
+
+export default Cart;
